c79e6f1db112a4ded148cd2a96caf8fb033bd647
uvr-argy <253669969+uvr-argy@users.noreply.github.com>
2026-01-21 09:39:06 +0100

    Commit para guardar todo antes de hacer el Merge -Unax

--- xampp/admin/index.php
+++ xampp/admin/index.php
@@ -4,4 +4,11 @@ session_start();
 $sesion = $_SESSION["sesion"] ?? "";
 if ($sesion == "") debug_header("Location: ../login.php");
 ?>
-<h1>Has iniciado sesión como administrador!<h1>
+<h1>Has iniciado sesión como administrador!</h1>
+<p>Cosas que hacer</p>
+<ul>
+    <li>Gestionar coches</li>
+    <li>Gestionar modelos</li>
+    <li>Gestionar sucursales</li>
+    <li>Gestionar usuarios</li>
+</ul>
