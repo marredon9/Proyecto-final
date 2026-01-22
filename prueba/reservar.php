07e850b114ca484dd53438602150e297030f8064
Ikasle Arangoya <ikasle@arangoya.local>
2026-01-22 14:11:37 +0100

    Cambios en el index -Marina

--- prueba/reservar.php
+++ prueba/reservar.php
@@ -7,13 +7,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fecha = $_POST['fecha'];
     $horario = $_POST['horario'];
 
-    // Aquí puedes guardar los datos en una base de datos o enviar un email
-    // Por ejemplo, enviar un email:
     $destinatario = "contacto@alquiza.com";
     $asunto = "Nueva reserva de $nombre";
     $mensaje = "Nombre: $nombre\nEmail: $email\nTeléfono: $telefono\nVehículo: $vehiculo\nFecha: $fecha\nHorario: $horario";
 
     $cabeceras = "From: no-reply@alquiza.com";
+
+    
+    $necesita_silla = isset($_POST['silla']) ? true : false;
+    $tiene_mascota = isset($_POST['mascota']) ? true : false;
 
     if (mail($destinatario, $asunto, $mensaje, $cabeceras)) {
         echo "Reserva enviada correctamente.";
