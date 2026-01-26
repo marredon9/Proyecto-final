3ea15b7ba5eb2ad7ddb67a62e3ab83e645b8300e
Marina <131159001+marredon9@users.noreply.github.com>
2026-01-26 14:08:40 +0100

    Añadir en el link php - Marina

--- prueba/index.php
+++ prueba/index.php
@@ -27,7 +27,7 @@
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav gap-3">
-                    <li class="nav-item"><a class="nav-link" href="#flota">Iniciar Sesion</a></li>
+                    <li class="nav-item"><a class="nav-link" href="IniciarSesion.php">Iniciar Sesion</a></li>
                     <li class="nav-item"><a class="nav-link" href="coches.php">Coches</a></li>
                     <li class="nav-item"><a class="nav-link" href="motos.php">Motos</a></li>
                     <li class="nav-item"><a class="nav-link" href="furgonetas.php">Furgonetas</a></li>
