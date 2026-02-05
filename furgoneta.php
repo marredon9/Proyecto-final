9a558e2b7cf11f349f08402ffa7d3130cbc3e911
Oier <194371770+0ier@users.noreply.github.com>
2026-02-05 09:52:57 +0100

    Cambiando la ruta del servidor -Oier

--- furgoneta.php
+++ furgoneta.php
@@ -15,5 +15,34 @@ gestionarModoOscuro();
 
 <body>
 
+<div class="card" style="width: 18rem;">
+            <img src="<?=img("van.png")?>"class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">Furgonetas</h5>
+                </div>
+                <a href="<?=lnk("buscar.php")?>" class="btn btn-outline-dark">Ver más</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <img src="<?=img("van.png")?>"class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">Furgonetas</h5>
+                </div>
+                <a href="<?=lnk("buscar.php")?>" class="btn btn-outline-dark">Ver más</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <img src="<?=img("van.png")?>"class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">Furgonetas</h5>
+                </div>
+                <a href="<?=lnk("buscar.php")?>" class="btn btn-outline-dark">Ver más</a>
+            </div>
+        </div>
     <!-- Navbar -->
     <?= navbar() ?>
--- include/rutas.php
+++ include/rutas.php
@@ -1,22 +1,22 @@
 <?php
 
-define("RUTA_ABS", "http://localhost/Proyecto_Final/");
-//define("RUTA_ABS", "http://localhost/Proyecto_Final/Alquiza/");
+//define("RUTA_ABS", "http://localhost/php/git/proyectofinal/Proyecto-final/");
+define("RUTA_ABS", "http://localhost/Proyecto-final/Proyecto-final/");
 //define("RUTA_ABS", "http://localhost/git/proyectofinal/");
-define("RUTA_IMG", RUTA_ABS . "assets/img/");
-define("RUTA_VID", RUTA_ABS . "assets/vid/");
-define("RUTA_SRV", RUTA_ABS . "servlets/");
+define("RUTA_IMG", "assets/img/");
+define("RUTA_VID", "assets/vid/");
+define("RUTA_SRV", "servlets/");
 
 function img($nombre) {
-    return RUTA_IMG . $nombre;
+    return RUTA_ABS . RUTA_IMG . $nombre;
 }
 
 function vid($nombre) {
-    return RUTA_VID . $nombre;
+    return RUTA_ABS . RUTA_VID . $nombre;
 }
 
 function srv($nombre) {
-    return RUTA_SRV . $nombre . ".php";
+    return RUTA_ABS . RUTA_SRV . $nombre . ".php";
 }
 
 function lnk($ruta) {
