89591633213fdfdb797ff42cdecb6a9ad21f90b4
Marina <131159001+marredon9@users.noreply.github.com>
2026-02-05 09:38:40 +0100

    Añadidos .php - Marina

---
+++ coche.php
@@ -0,0 +1,63 @@
+<?php
+// Iniciar sesión o verificar si hay una cookie de tema
+include "include.php";
+session_start();
+
+gestionarModoOscuro();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?=headerPagina()?>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <?= navbar() ?>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <img src="img/coches/500.png" class="card-img-top" alt="fiat 500">
+                <div class="card-body">
+                    <p class="card-text"></p>
+                </div>
+                <div class="card-footer text-muted">Card footer</div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Texto de ejemplo 2.</p>
+                </div>
+                <div class="card-footer text-muted">Card footer</div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Texto de ejemplo 3.</p>
+                </div>
+                <div class="card-footer text-muted">Card footer</div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Texto de ejemplo 4.</p>
+                </div>
+                <div class="card-footer text-muted">Card footer</div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ furgoneta.php
@@ -0,0 +1,19 @@
+<?php
+// Iniciar sesión o verificar si hay una cookie de tema
+include "include.php";
+session_start();
+
+gestionarModoOscuro();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?=headerPagina()?>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <?= navbar() ?>
