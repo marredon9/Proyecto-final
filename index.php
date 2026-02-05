a8d3825a3868afc1790a6d0111ecce56c27295fe
tralli18 <24dw.illart.beain@arangoya.net>
2026-02-05 14:09:24 +0100

    Eliminar en el administrador php  y cambiar el navbar de administrador -Illart

--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-    <link src="Css/index.css">
-</head>
-
-<body>
-    <nav id="navbar" class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand">Navbar</a>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
-</body>
-=======
 <?php
 // Iniciar sesión o verificar si hay una cookie de tema
 include "include.php";
@@ -202,6 +177,5 @@ gestionarModoOscuro();
 </body>
 <!-- Leaflet JS -->
 
->>>>>>> 2ddee589a854d8b6d13b88dd6b0c335575383f60
 
 </html>
