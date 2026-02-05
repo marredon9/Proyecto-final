b05e0a34220ff677c4cdbca903574d8ba5b9ed15
Marina <131159001+marredon9@users.noreply.github.com>
2026-02-05 11:01:46 +0100

    terminado coche.php -Marina

--- coche.php
+++ coche.php
@@ -19,7 +19,7 @@ gestionarModoOscuro();
     <?= navbar() ?>
 
     <h2 class="section-title mb-4 text-center mt-3" id="flota">Nuestros coches</h2>
-    <div class="d-flex justify-content-center gap-3 flex-wrap align-items-stretch">
+    <div class="d-flex justify-content-center gap-3 flex-wrap align-items-stretch" style="padding-bottom: 30px;">
         <div class="card" style="width: 18rem;">
             <img src="<?= img("focus (2).png") ?>" class="card-img-top" alt="coche">
             <div class="card-body d-flex flex-column justify-content-between">
@@ -41,32 +41,72 @@ gestionarModoOscuro();
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="<?= img("van.png") ?>" class="card-img-top" alt="...">
+            <img src="<?= img("citroen.png") ?>" class="card-img-top" alt="coche">
             <div class="card-body d-flex flex-column justify-content-between">
                 <div>
-                    <h5 class="card-title">Furgonetas</h5>
+                    <h5 class="card-title">Citroen e-C4</h5>
                 </div>
-                <a href="<?= lnk("furgoneta.php") ?>" class="btn btn-outline-dark">Ver más</a>
+                <span class="text-card"><img src="<?= img("enchufe.png") ?>" style="width: 20px;"> Eléctrico <img
+                        src="<?= img("marchas.png") ?>" style="width: 20px;">Automático <img
+                        src="<?= img("grupo.png") ?>" style="width: 20px;"> 5 Personas <img
+                        src="<?= img("maleta.png") ?>" style="width: 20px;"> 2 Maletas</span>
+                <h5 style="margin-top: 10px;">Características del vehículo</h5>
+
+                <li> Radio estéreo AM/FM</li>
+                <li> Airbags</li>
+                <li> Rango de conducción estimado hasta 175 millas</li>
+                <li> Aire acondicionado</li>
+                <li> Vehículo eléctrico</li>
+            </div>
+            <div class="card-footer">
+                <p>25€/día</p>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="<?= img("moto.png") ?>" class="card-img-top" alt="...">
+            <img src="<?= img("peugeot.png") ?>" class="card-img-top" alt="coche">
             <div class="card-body d-flex flex-column justify-content-between">
                 <div>
-                    <h5 class="card-title">Motos</h5>
+                    <h5 class="card-title">Peugeot 508 SW </h5>
                 </div>
-                <a href="<?= lnk("moto.php") ?>" class="btn btn-outline-dark">Ver más</a>
+                <span class="text-card"><img src="<?= img("enchufe.png") ?>" style="width: 20px;"> Eléctrico <img
+                        src="<?= img("marchas.png") ?>" style="width: 20px;">Automático <img
+                        src="<?= img("grupo.png") ?>" style="width: 20px;"> 5 Personas <img
+                        src="<?= img("maleta.png") ?>" style="width: 20px;"> 3 Maletas</span>
+                <h5 style="margin-top: 10px;">Características del vehículo</h5>
+
+                <li>Radio estéreo AM/FM</li>
+                <li> Airbags</li>
+                <li> Vehículo de gasolina</li>
+                <li> Aire acondicionado</li>
+            </div>
+            <div class="card-footer">
+                <p>25€/día</p>
             </div>
         </div>
         <div class="card" style="width: 18rem;">
-            <img src="<?= img("moto.png") ?>" class="card-img-top" alt="...">
+            <img src="<?= img("descapotable.png") ?>" class="card-img-top" alt="coche">
             <div class="card-body d-flex flex-column justify-content-between">
                 <div>
-                    <h5 class="card-title">Motos</h5>
+                    <h5 class="card-title">Audi A5 </h5>
                 </div>
-                <a href="<?= lnk("moto.php") ?>" class="btn btn-outline-dark">Ver más</a>
+                <span class="text-card"><img src="<?= img("marchas.png") ?>" style="width: 20px;">Automático <img
+                        src="<?= img("grupo.png") ?>" style="width: 20px;"> 4 Personas <img
+                        src="<?= img("maleta.png") ?>" style="width: 20px;"> 1 Maletas</span>
+                <h5 style="margin-top: 10px;">Características del vehículo</h5>
+
+                <li>Radio estéreo AM/FM</li>
+                <li> Airbags</li>
+                <li> Vehículo de gasolina</li>
+                <li> Aire acondicionado</li>
+            </div>
+            <div class="card-footer">
+                <p>25€/día</p>
             </div>
         </div>
+
     </div>
     </div>
     <?= footer() ?>
+</body>
+
+</html>
--- moto.php
+++ moto.php
@@ -10,7 +10,7 @@ gestionarModoOscuro();
 <html lang="es">
 
 <head>
-    <?=headerPagina()?>
+    <?= headerPagina() ?>
 </head>
 
 <body>
@@ -18,4 +18,95 @@ gestionarModoOscuro();
     <!-- Navbar -->
     <?= navbar() ?>
 
-    
+    <h2 class="section-title mb-4 text-center mt-3" id="flota">Nuestros coches</h2>
+    <div class="d-flex justify-content-center gap-3 flex-wrap align-items-stretch" style="padding-bottom: 30px;">
+        <div class="card" style="width: 18rem;">
+            <img src="<?= img("focus (2).png") ?>" class="card-img-top" alt="coche">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">Ford Focus</h5>
+                </div>
+                <span class="text-card"><img src="<?= img("marchas.png") ?>" style="width: 20px;">Automático <img
+                        src="<?= img("grupo.png") ?>" style="width: 20px;"> 5 Personas <img
+                        src="<?= img("maleta.png") ?>" style="width: 20px;"> 2 Maletas</span>
+                <h5 style="margin-top: 10px;">Características del vehículo</h5>
+
+                <li>Radio estéreo AM/FM</li>
+                <li> Airbags</li>
+                <li> Vehículo de gasolina</li>
+                <li> Aire acondicionado</li>
+            </div>
+            <div class="card-footer">
+                <p>20€/día</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="<?= img("citroen.png") ?>" class="card-img-top" alt="coche">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">Citroen e-C4</h5>
+                </div>
+                <span class="text-card"><img src="<?= img("enchufe.png") ?>" style="width: 20px;"> Eléctrico <img
+                        src="<?= img("marchas.png") ?>" style="width: 20px;">Automático <img
+                        src="<?= img("grupo.png") ?>" style="width: 20px;"> 5 Personas <img
+                        src="<?= img("maleta.png") ?>" style="width: 20px;"> 2 Maletas</span>
+                <h5 style="margin-top: 10px;">Características del vehículo</h5>
+
+                <li> Radio estéreo AM/FM</li>
+                <li> Airbags</li>
+                <li> Rango de conducción estimado hasta 175 millas</li>
+                <li> Aire acondicionado</li>
+                <li> Vehículo eléctrico</li>
+            </div>
+            <div class="card-footer">
+                <p>25€/día</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="<?= img("peugeot.png") ?>" class="card-img-top" alt="coche">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">Peugeot 508 SW </h5>
+                </div>
+                <span class="text-card"><img src="<?= img("enchufe.png") ?>" style="width: 20px;"> Eléctrico <img
+                        src="<?= img("marchas.png") ?>" style="width: 20px;">Automático <img
+                        src="<?= img("grupo.png") ?>" style="width: 20px;"> 5 Personas <img
+                        src="<?= img("maleta.png") ?>" style="width: 20px;"> 3 Maletas</span>
+                <h5 style="margin-top: 10px;">Características del vehículo</h5>
+
+                <li>Radio estéreo AM/FM</li>
+                <li> Airbags</li>
+                <li> Vehículo de gasolina</li>
+                <li> Aire acondicionado</li>
+            </div>
+            <div class="card-footer">
+                <p>25€/día</p>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="<?= img("descapotable.png") ?>" class="card-img-top" alt="coche">
+            <div class="card-body d-flex flex-column justify-content-between">
+                <div>
+                    <h5 class="card-title">Audi A5 </h5>
+                </div>
+                <span class="text-card"><img src="<?= img("marchas.png") ?>" style="width: 20px;">Automático <img
+                        src="<?= img("grupo.png") ?>" style="width: 20px;"> 4 Personas <img
+                        src="<?= img("maleta.png") ?>" style="width: 20px;"> 1 Maletas</span>
+                <h5 style="margin-top: 10px;">Características del vehículo</h5>
+
+                <li>Radio estéreo AM/FM</li>
+                <li> Airbags</li>
+                <li> Vehículo de gasolina</li>
+                <li> Aire acondicionado</li>
+            </div>
+            <div class="card-footer">
+                <p>25€/día</p>
+            </div>
+        </div>
+
+    </div>
+    </div>
+    <?= footer() ?>
+</body>
+
+</html>
