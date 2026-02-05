02e8418b75633e287f797f72a063e2b05b40d0a2
Unax <253669969+uvr-argy@users.noreply.github.com>
2026-02-05 21:06:13 +0100

    Cambiadas direcciones IP de base de datos y local para facilitar despliegue -Unax

--- include/rutas.php
+++ include/rutas.php
@@ -1,11 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-define("RUTA_ABS", "http://localhost/Proyecto-final/Proyecto-final/");
-=======
-//define("RUTA_ABS", "http://localhost/Proyecto_Final/");
-define("RUTA_ABS", "http://localhost/php/git/proyectofinal/Proyecto-final/");
->>>>>>> proto
+define("RUTA_ABS", "http://localhost/Proyecto_Final/");
+//define("RUTA_ABS", "http://localhost/php/git/proyectofinal/Proyecto-final/");
 //define("RUTA_ABS", "http://localhost/Proyecto_Final/Alquiza/");
 //define("RUTA_ABS", "http://localhost/git/proyectofinal/");
 define("RUTA_IMG", RUTA_ABS . "assets/img/");
